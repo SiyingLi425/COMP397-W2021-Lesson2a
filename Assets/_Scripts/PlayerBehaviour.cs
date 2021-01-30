c7d88a1fdb4cfd7a0568d386c1fe14000b2babfa
SIying Li <yukimashiro425@gmail.com>
2021-01-29 22:29:02 -0500

    Added Camera and FPS Character Controller

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -4,27 +4,56 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    public float movementForce;
-    public Rigidbody rigidbody;
+    public CharacterController controller;
+
+    public float maxSpeed = 10.0f;
+    public float gravity = -30.0f;
+    public float jumpHeight = 3.0f;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.5f;
+    public LayerMask groundMask;
+
+    public Vector3 velocity;
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            //move right
-            rigidbody.AddForce(Vector3.right * movementForce);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundMask);
 
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2.0f;
         }
-        if(Input.GetAxisRaw("Horizontal") < 0){
-            //move left
-            rigidbody.AddForce(Vector3.left * movementForce);
-        }  
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * maxSpeed * Time.deltaTime);
+
+        if(Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
+
+    //only visible in editor
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+    }
+
 }
